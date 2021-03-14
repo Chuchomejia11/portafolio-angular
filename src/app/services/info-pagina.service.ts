dcb34178fe89389badb361bf40e0488deb8e2176
Jesus Mejia <Chuchomejia11@users.noreply.github.com>
2021-03-14 15:19:36 -0600

    Fin de la leccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -29,7 +29,7 @@ export class InfoPaginaService {
     this.http.get('https://angular-html-2512d-default-rtdb.firebaseio.com/equipo.json').subscribe((resp:any[]) => {
  
       this.equipo= resp;
-      console.log(resp)
+      //console.log(resp)
       }); 
   }
 }
